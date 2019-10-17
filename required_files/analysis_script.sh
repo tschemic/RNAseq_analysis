8cbb9a45623138cb576255d62b67d09280e0abfc
Michael Tscherner <48102019+tschemic@users.noreply.github.com>
2019-10-17 14:49:21 +0200

    Update analysis_script.sh

--- required_files/analysis_script.sh
+++ required_files/analysis_script.sh
@@ -16,6 +16,10 @@ read -p 'Do you want to do a quality control of the raw data (yes or no): ' QCRA
 
 read -p 'Are the libraries prepared in a strand-specific way? (yes or no): ' STRANDED
 
+read -p 'Are the data paired-end? (yes or no): ' PAIRED
+
+read -p 'How many threads should be used for the analysis (use 1, if you are not sure): ' THREAD
+
 if [ $GENEDATA == 'yes' ]
 then
 	echo 'Retrieving genomic data from CGD.'
@@ -34,10 +38,38 @@ fi
 GENOME=$WKDIR/required_files/C_albicans_SC5314_A22_current_chromosomesAM.fasta
 FEATURES=$WKDIR/required_files/C_albicans_SC5314_A22_current_features_haploid.gff
 ADAPT1=$(cat $WKDIR/required_files/config_file.txt | grep Read1: | cut -d ":" -f 2)
+#ADAPT2=$(cat $WKDIR/required_files/config_file.txt | grep Read2: | cut -d ":" -f 2) # uncomment this for paired-end data
 rRNA=$WKDIR/required_files/Ca_A22chrAM_rRNAloci.bed
 mkdir $WKDIR/QC
 PICARD=$WKDIR/required_files/picard.jar
 
+
+# QC of raw data
+
+if [ $QCRAW == 'yes' ]
+then
+	mkdir $WKDIR/QC_raw
+	echo 'Quality control of raw data:'
+	if [ $FORMAT == 'bam' ]
+	then
+		for i in $WKDIR/*.bam
+		do
+			fastqc -o $WKDIR/QC_raw $i
+		done
+	else
+		for SNAME in $(ls $WKDIR | egrep '(\.f.*q$)|(q\.gz$)')
+		do
+			i=$WKDIR/$SNAME
+			fastqc -o $WKDIR/QC_raw $i
+		done
+	fi
+	multiqc -o $WKDIR/QC_raw $WKDIR/QC_raw
+else
+	echo 'No QC of raw data done.'
+fi
+
+
+# Convert .bam to .fastq format
 
 if [ $FORMAT == 'bam' ]
 then
@@ -54,47 +86,42 @@ else
 	exit
 fi
 
-# QC of raw data
-
-if [ $QCRAW == 'yes' ]
-then
-	echo 'Quality control of raw data:'
-	#for i in $WKDIR/$(ls $WKDIR | egrep '(\.f.*q$)|(q\.gz$)')
-	for i in $WKDIR/*.fq
-	do
-		fastqc -o $WKDIR/QC $i
-	done
-else
-	echo 'No QC of raw data done.'
-fi
 
 # Adapter removal with cutadapt and mapping of all files with NGM
 
-#for i in $WKDIR/$(ls $WKDIR | egrep '(\.f.*q$)|(q\.gz$)')
-for i in $WKDIR/*.fq
+for SNAME in $(ls $WKDIR | egrep '(\.f.*q$)|(q\.gz$)')
 do
-	SNAME=$(echo $i | sed 's:/.*/::g')
-	cutadapt -q 30 -a $ADAPT1 $i > $i.trimmed.fq 2>$WKDIR/QC/$SNAME.cutadapt.report.txt   # removes Illumina TrueSeq adapters from reads (change -a for different adapters); -j specifies number of cores to use, remove if not sure
+	i=$WKDIR/$SNAME # sets sample name and file path
+	
+	if [ $PAIRED == 'yes' ]
+	then
+		cutadapt --interleaved -q 30 -j $THREAD -O 1 -a $ADAPT1 -A $ADAPT2 $i > $i.trimmed.fq 2>$WKDIR/QC/Cutadapt_$SNAME.txt  # removes adapters from reads (change adapter sequences in config_file.txt if necessary);
+	else
+		cutadapt -q 30 -j $THREAD -O 1 -a $ADAPT1 $i > $i.trimmed.fq 2>$WKDIR/QC/Cutadapt_$SNAME.txt
+	fi
 	rm $i
 
-	ngm -q $i.trimmed.fq -r $GENOME -o $i.trimmed.fq.bam -b  # add -p for paired-end data; -t 6 is optional - means 6 threads of the processor are used, if you don't know what to do, remove it; --topn 1 --strata causes ngm to write only uniquely mapping reads to the output
+	if [ $PAIRED == 'yes' ]
+	then
+		ngm -q $i.trimmed.fq -r $GENOME -o $i.trimmed.fq.bam -p -b -Q 30 -t $THREAD # add -p for paired-end data; -t 6 is optional - means 6 threads of the processor are used, if you don't know what to do, remove it; --topn 1 --strata causes ngm to write only uniquely mapping reads to the output
+	else
+		ngm -q $i.trimmed.fq -r $GENOME -o $i.trimmed.fq.bam -b -Q 30 -t $THREAD
+	fi
 	rm $i.trimmed.fq
 
-	samtools sort $i.trimmed.fq.bam -o $i.trimmed.fq.bam.sort.bam   # sort .bam files using samtools
+	samtools sort -@ $THREAD $i.trimmed.fq.bam -o $i.trimmed.fq.bam.sort.bam   # sort .bam files using samtools
 	rm $i.trimmed.fq.bam
 
 	bedtools intersect -a $i.trimmed.fq.bam.sort.bam -b $rRNA -v > $i.trimmed.fq.bam.sort.bam.rRNAfilt.bam  # removal of reads mapping to rRNA loci
 	rm $i.trimmed.fq.bam.sort.bam
 
-	#samtools rmdup -s $i.trimmed.fq.bam.sort.bam.rRNAfilt.bam $i.trimmed.fq.bam.sort.bam.rRNAfilt.bam.rmdup.bam  # removal of duplicated reads
-
 	# Labelling of duplicated reads and removal of optical duplicates
-	java -jar $PICARD MarkDuplicates REMOVE_SEQUENCING_DUPLICATES=true I=$i.trimmed.fq.bam.sort.bam.rRNAfilt.bam O=$i.trimmed.fq.bam.sort.bam.rRNAfilt.bam.markdup.bam M=$WKDIR/QC/$SNAME.markdup.metrics.txt
+	java -jar $PICARD MarkDuplicates REMOVE_SEQUENCING_DUPLICATES=true I=$i.trimmed.fq.bam.sort.bam.rRNAfilt.bam O=$i.final.bam M=$WKDIR/QC/$SNAME.markdup.metrics.txt
 
-	#echo $i >> $WKDIR/QC/flagstat_analysis.txt
-	samtools flagstat $i.trimmed.fq.bam.sort.bam.rRNAfilt.bam.markdup.bam >> $WKDIR/QC/$SNAME.markdup.flagstat_analysis.txt   # flagstat analysis
+	#Quality control and statistics about mapped samples
+	samtools flagstat $i.final.bam >> $WKDIR/QC/$SNAME.final.flagstat_analysis.txt   # flagstat analysis
 
-	fastqc -o $WKDIR/QC $i.trimmed.fq.bam.sort.bam.rRNAfilt.bam.markdup.bam
+	fastqc -o $WKDIR/QC $i.final.bam
 
 done
 
@@ -104,20 +131,30 @@ multiqc -s -o $WKDIR/QC $WKDIR/QC
 
 mkdir $WKDIR/IGV_files
 
-for i in $WKDIR/*.markdup.bam
+for i in $WKDIR/*.final.bam
 do
 	samtools index $i
 	SNAME=$(echo $i | sed 's:/.*/::g')
-	bamCoverage -b $i -o $WKDIR/IGV_files/$SNAME.bw  --normalizeUsing CPM
+	if [ $PAIRED == 'yes' ]
+	then
+		bamCoverage -b $i -o $WKDIR/IGV_files/$SNAME.bw --normalizeUsing CPM -bs 5 -p $THREAD -e
+	else
+		bamCoverage -b $i -o $WKDIR/IGV_files/$SNAME.bw --normalizeUsing CPM -bs 5 -p $THREAD
+	fi
 done
 
 
 
 mkdir $WKDIR/count
 
-for i in $WKDIR/*.markdup.bam
+for i in $WKDIR/*.final.bam
 do
-	htseq-count -f bam -s $STRANDED -t gene -i ID $i $FEATURES > $i.count.txt  # read count  for each gene with htseq-count
+	if [ $PAIRED == 'yes' ]
+	then
+		htseq-count -f bam -s $STRANDED -r pos -t gene -i ID $i $FEATURES > $i.count.txt  # read count  for each gene with htseq-count
+	else
+		htseq-count -f bam -s $STRANDED -t gene -i ID $i $FEATURES > $i.count.txt
+	fi
 	mv $i.count.txt $WKDIR/count
 done
 
